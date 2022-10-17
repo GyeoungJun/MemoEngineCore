a02dd28eb9b41474dbc9fc7b28f048b4056e8965
82107479 <corona.kim@kt.com>
2022-10-17 16:55:53 +0900

    ASP.NET & Core를 다루는 기술 - 3장. ASP.NET 웹 폼(DevASPNET)과 윈 폼(DevWindowsForms)

---
+++ DevWindowsForms/FrmWindowsForms.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevWindowsForms
+{
+    public partial class FrmWindowsForms : Form
+    {
+        public FrmWindowsForms()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmWindowsForms_Load(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.Millisecond.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = $"{textBox1.Text} - {DateTime.Now.Millisecond}";
+        }
+    }
+}
